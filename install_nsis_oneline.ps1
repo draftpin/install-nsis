aaab6fd3998c2f9a928b78e6bb9c81541211923b
Ergamon <draftpin@gmail.com>
2025-04-30 20:45:02 +0300

    Fix PATH for new terminal start

--- install_nsis_oneline.ps1
+++ install_nsis_oneline.ps1
@@ -1,1 +1,1 @@
- choco install nsis -y; $addPath='C:\Program Files (x86)\NSIS'; if (-not ($env:PATH.Split(';') -contains $addPath)) { $env:PATH+=";$addPath" } 
+ choco install nsis -y; $addPath='C:\Program Files (x86)\NSIS'; if (-not ($env:PATH.Split(';') -contains $addPath)) { $env:PATH+=";$addPath"; setx PATH $env:PATH } 
